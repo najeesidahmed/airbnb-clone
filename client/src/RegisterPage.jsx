31d1112014aade74349a0f0e47f62be0aa1ab416
najee sidahmed <najeekamil@msn.com>
2023-03-20 12:05:36 +0000

    connect and test mongo database

--- client/src/RegisterPage.jsx
+++ client/src/RegisterPage.jsx
@@ -7,13 +7,18 @@ export default function RegisterPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    function registerUser(e){
+    async function registerUser(e){
         e.preventDefault();
-        axios.post('/register', {
-            name,
-            email, 
-            password,
-        });
+        try {
+            await axios.post('/register', {
+                name,
+                email, 
+                password,
+            });
+            alert('Sign up successful. Now you can log in')
+        } catch (error) {
+            alert('Sign up unsuccessful. Please refresh and try again')
+        }
     }
     return (
         <div className="mt-4 grow flex items-center justify-around">
